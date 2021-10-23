64d3bdd8002bbf8a914a3982804fa1814d3c406f
Quang Nguyen <quangnguyen@Quangs-Mac-mini.local>
2021-10-22 20:34:53 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Box = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <div className="red-square"></div>
+      <p className="text-bold">#FF6663</p>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div>
+      <Box />
+    </div>
+  );
+};
 
 export default App;
